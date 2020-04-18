7ae730393491ce035cd60fac4334e6df8bbf916a
PapitoTI <42822804+PapitoTI@users.noreply.github.com>
2020-04-18 00:18:18 -0200

    Add files via upload

---
+++ Testes/Nac/src/br/com/fiap/TesteJulgamentoPrisioneiro.java
@@ -0,0 +1,26 @@
+package br.com.fiap;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import br.com.fiap.JulgamentoPrisioneiro;
+
+public class TesteJulgamentoPrisioneiro extends JulgamentoPrisioneiro{
+
+	@Test
+	public void testeCalcularPena() {
+		
+		int PENA_INOCENCIA = 0;
+		int PENA_CONDENACAO_MUTUA = 5;
+		int PENA_CONDENACAO_INDIVIDUAL = 10;
+		int PENA_CONDENACAO_CUMPLICES = 1;
+		
+		JulgamentoPrisioneiro j = new JulgamentoPrisioneiro();
+		
+		int ex = PENA_CONDENACAO_MUTUA;
+		int ac = j.calculaPena(Resposta.DELACAO_C, Resposta.DELACAO_C);
+		
+		assertEquals(ex, ac);
+		
+	}
+}
